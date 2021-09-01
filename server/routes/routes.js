85f09cd5b51ae2275cf05cd56f107d81c2d40735
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2021-09-02 00:16:51 +0600

    change in router.js post api

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -10,12 +10,6 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', upload.single('image'), (req, res, next) => {
-    var img = fs.readFileSync(req.file.path);
-    var encode_img = img.toString('base64');
-    var final_img = {
-        contentType: req.file.mimetype,
-        image: new Buffer(encode_img, 'base64')
-    };
 
 });
 
