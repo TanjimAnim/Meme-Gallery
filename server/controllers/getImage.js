ad5b030a67c6fddc7f680bfffc44f365de97477b
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2021-09-06 02:05:56 +0600

    controller for displaing images

---
+++ server/controllers/getImage.js
@@ -0,0 +1,20 @@
+//  the GET request handler that provides the images
+
+const imageModel = require("../models/models.js");
+
+const getImages = (req, res) => {
+  imageModel
+    .find({}, (err, img) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("An error occurred", err);
+      } else {
+        console.log(img);
+        res.contentType("json");
+        res.send(img);
+      }
+    })
+    .sort({ createdAt: "desc" });
+};
+
+module.exports = getImages;
