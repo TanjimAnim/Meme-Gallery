1395da4b3e7214a46d3912b3927e25554bba105b
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2021-09-06 02:06:48 +0600

    controller for saving image from URL

---
+++ server/controllers/submitLink.js
@@ -0,0 +1,21 @@
+// post api for saving images from URL
+
+const imageModel = require("../models/models.js");
+
+var date = new Date();
+var currentDate = date.toLocaleDateString();
+
+const submitLink = async (req, res) => {
+  let saveImageFromUrl = new imageModel();
+  saveImageFromUrl.img.data = req.body.url;
+  saveImageFromUrl.postedAt = currentDate;
+  await saveImageFromUrl.save(function (err, result) {
+    if (err) return console.error(err);
+    else {
+      console.log(result);
+      res.sendStatus(202);
+    }
+  });
+};
+
+module.exports = submitLink;
