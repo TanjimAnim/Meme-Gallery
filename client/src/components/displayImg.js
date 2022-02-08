aa1cda70b99f9165afd6e867b7c3c5376f68a89e
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-02-09 01:00:37 +0600

    minor change

--- client/src/components/displayImg.js
+++ client/src/components/displayImg.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { Box, Image, CloseButton, Flex } from "@chakra-ui/react";
+import { Box, Image, CloseButton, Flex, Center } from "@chakra-ui/react";
 import { baseUrl } from "../config";
 
 class DisplayImage extends React.Component {
@@ -35,27 +35,33 @@ class DisplayImage extends React.Component {
               mb={3}
               width={{ base: "90%", md: "50%", xl: "30%" }}
             >
-              <Image
-                src={`${img.img.data}`}
-                fit='cover'
-                borderRadius={3}
-                boxSize={{ base: "100%", md: "60%", xl: "70%" }}
-              />
-              <CloseButton
-                size='sm'
-                onClick={() => {
-                  this.onDelete(img._id);
-                }}
-                style={{
-                  position: "absolute",
-                  top: "0%",
-                  left: "0%",
-                  width: 20,
-                  height: 20,
-                  color: "black",
-                  background: "#FFFFF0",
-                }}
-              />
+              <Center>
+                <Image
+                  src={`${img.img.data}`}
+                  fit='cover'
+                  borderRadius={3}
+                  boxSize={{ base: "100%", md: "50%", xl: "70%" }}
+                />
+
+                <CloseButton
+                  size='sm'
+                  onClick={() => {
+                    this.onDelete(img._id);
+                  }}
+                  style={{
+                    position: "absolute",
+                    top: "0%",
+                    left: "25%",
+                    width: 20,
+                    height: 20,
+                    color: "black",
+                    background: "#FFFFF0",
+                    hover: {
+                      background: "teal",
+                    },
+                  }}
+                />
+              </Center>
             </Box>
           );
         })}
