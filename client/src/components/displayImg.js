5bec218c120f99af9a420e3f500798b0a65bbf81
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-02-24 20:57:37 +0600

    changing class component to functional component

--- client/src/components/displayImg.js
+++ client/src/components/displayImg.js
@@ -3,71 +3,69 @@ import axios from "axios";
 import { Box, Image, CloseButton, Flex, Center } from "@chakra-ui/react";
 import { baseUrl } from "../config";
 
-class DisplayImage extends React.Component {
-  onDelete = (id) => {
+export default function DisplayImage(props) {
+  const onDelete = (id) => {
     axios
       .post(`${baseUrl}/delete/${id}`)
       .then((response) => {
-        this.props.onSuccessfulUpload();
+        props.onSuccessfulUpload();
       })
       .catch(function (error) {
         console.log(error);
       });
   };
 
-  render() {
-    return (
-      <Flex
-        flexDirection='row'
-        alignItems='center'
-        justifyContent='space-evenly'
-        wrap='wrap'
-        mt={3}
-      >
-        {this.props.images.map((img) => {
-          return (
-            <Box
-              position='relative'
-              mt={4}
-              ml={2}
-              mx='auto'
-              my='auto'
-              mb={3}
-              width={{ base: "90%", md: "50%", xl: "30%" }}
-            >
-              <Center>
-                <Image
-                  src={`${img.img.data}`}
-                  fit='cover'
-                  borderRadius={3}
-                  boxSize={{ base: "100%", md: "50%", xl: "70%" }}
-                />
+  return (
+    <Flex
+      flexDirection='row'
+      alignItems='center'
+      justifyContent='space-evenly'
+      wrap='wrap'
+      mt={3}
+    >
+      {props.images.map((img) => {
+        return (
+          <Box
+            key={img._id}
+            position='relative'
+            mt={4}
+            ml={2}
+            mx='auto'
+            my='auto'
+            mb={3}
+            width={{ base: "90%", md: "50%", xl: "30%" }}
+          >
+            <Center>
+              <Image
+                src={`${img.img.data}`}
+                fit='cover'
+                borderRadius={3}
+                boxSize={{ base: "100%", md: "50%", xl: "70%" }}
+              />
 
-                <CloseButton
-                  size='sm'
-                  onClick={() => {
-                    this.onDelete(img._id);
-                  }}
-                  style={{
-                    position: "absolute",
-                    top: "0%",
-                    left: "25%",
-                    width: 20,
-                    height: 20,
-                    color: "black",
-                    background: "#FFFFF0",
-                    hover: {
-                      background: "teal",
-                    },
-                  }}
-                />
-              </Center>
-            </Box>
-          );
-        })}
-      </Flex>
-    );
-  }
+              <CloseButton
+                size='sm'
+                left={{ base: "0%", md: "25%", xl: "15%" }}
+                onClick={() => {
+                  onDelete(img._id);
+                }}
+                style={{
+                  position: "absolute",
+                  top: "0%",
+
+                  width: 20,
+
+                  color: "black",
+                  background: "#FFFFF0",
+                  hover: {
+                    background: "teal",
+                  },
+                }}
+              />
+            </Center>
+          </Box>
+        );
+      })}
+    </Flex>
+  );
 }
-
-export default DisplayImage;
