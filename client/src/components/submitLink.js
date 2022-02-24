cc56fb061c9585d7d201a73eb27764c456377325
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-02-24 20:57:58 +0600

    changing class component to functional component

--- client/src/components/submitLink.js
+++ client/src/components/submitLink.js
@@ -1,17 +1,11 @@
 import { FormControl, Input, Button } from "@chakra-ui/react";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { baseUrl } from "../config";
 
-class SubmitLink extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      url: "",
-    };
-  }
-  isUrlValid = () => {
-    const { url } = this.state;
+export default function SubmitLink(props) {
+  const [url, setUrl] = useState("");
+  const isUrlValid = () => {
     const res = url.match(
       /(http(s)?:\/\/.)?(www\.)?[\w#%+.:=@~-]{2,256}\.[a-z]{2,6}\b([\w#%&+./:=?@~-]*)/g
     );
@@ -21,61 +15,53 @@ class SubmitLink extends React.Component {
     return false;
   };
 
-  onInputChange = (event) => {
-    this.setState({
-      url: event.target.value,
-    });
+  const onInputChange = (event) => {
+    setUrl(event.target.value);
   };
 
-  onClick = () => {
+  const onClick = () => {
     axios
       .post(`${baseUrl}/submitlink`, {
-        url: this.state.url,
+        url: url,
       })
       .then((response) => {
         console.log(response);
-        this.props.onSuccessfulUpload();
-        this.setState({
-          url: "",
-        });
+        props.onSuccessfulUpload();
+        setUrl("");
       })
       .catch(function (error) {
         console.log(error);
       });
   };
 
-  render() {
-    return (
-      <FormControl>
-        <Input
-          type='url'
-          value={this.state.url}
-          onChange={this.onInputChange}
-          width={{
-            base: "70%",
-            md: "55%",
-            xl: "60%",
-          }}
-          ml={{
-            base: "5%",
-            md: "5%",
-            xl: "4%",
-          }}
-        />
-        <Button
-          ml={2}
-          mb={1}
-          colorScheme='teal'
-          type='submit'
-          onClick={this.onClick}
-          size='md'
-          isDisabled={this.isUrlValid()}
-        >
-          Submit
-        </Button>
-      </FormControl>
-    );
-  }
+  return (
+    <FormControl>
+      <Input
+        type='url'
+        value={url}
+        onChange={onInputChange}
+        width={{
+          base: "70%",
+          md: "55%",
+          xl: "60%",
+        }}
+        ml={{
+          base: "5%",
+          md: "5%",
+          xl: "4%",
+        }}
+      />
+      <Button
+        ml={2}
+        mb={1}
+        colorScheme='teal'
+        type='submit'
+        onClick={onClick}
+        size='md'
+        isDisabled={isUrlValid()}
+      >
+        Submit
+      </Button>
+    </FormControl>
+  );
 }
-
-export default SubmitLink;
