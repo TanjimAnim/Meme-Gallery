d3d4d15df22bfac68918cf95df56664cb235f07e
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-03-03 14:02:04 +0600

    async function created,changed error logic

--- client/src/components/upload.js
+++ client/src/components/upload.js
@@ -3,44 +3,14 @@ import axios from "axios";
 import { Button } from "@chakra-ui/react";
 import { baseUrl } from "../config";
 
-// function FileData(props) {
-//   if (props.selectedFile) {
-//     if (
-//       props.selectedFile.type === "image/jpeg" ||
-//       props.selectedFile.type === "image/png"
-//     ) {
-//       return (
-//         <div>
-//           {props.error && <div>file too large!!</div>}
-//           <Button colorScheme='teal' size='sm' onClick={props.fileUpload()}>
-//             Upload!
-//           </Button>
-//         </div>
-//       );
-//     } else {
-//       return (
-//         <div>
-//           <h4>Please choose an image to upload</h4>
-//         </div>
-//       );
-//     }
-//   } else {
-//     return (
-//       <div>
-//         <h4>Choose Photos</h4>
-//       </div>
-//     );
-//   }
-// }
-
 export default function Upload(props) {
   const [selectedFile, setSelectedFile] = useState(null);
   const [error, setError] = useState(null);
 
-  const fileUploadHandler = () => {
+  const fileUploadHandler = async () => {
     const fd = new FormData();
     fd.append("image", selectedFile, selectedFile.name);
-    axios
+    await axios
       .post(`${baseUrl}/upload`, fd)
       .then((res) => {
         props.onSuccessfulUpload();
@@ -64,10 +34,13 @@ export default function Upload(props) {
       (selectedFile.type === "image/jpeg" ||
         selectedFile.type === "image/png") ? (
         <div>
-          {error && <div>file too large!!</div>}
-          <Button colorScheme='teal' size='sm' onClick={fileUploadHandler}>
-            Upload!
-          </Button>
+          {error == null ? (
+            <Button colorScheme='teal' size='sm' onClick={fileUploadHandler}>
+              Upload!
+            </Button>
+          ) : (
+            <div>{error}</div>
+          )}
         </div>
       ) : (
         <div>
